8d11d588fd54f34727865c578f7649b39243535c
Muhammad Zainul Ahda <zainahda@gmail.com>
2021-04-01 16:26:25 +0700

    finishing Jumbotron

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,7 @@
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
           <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" variant="success" type="submit">Search</b-button>
+          <b-button size="sm" class="btn-search my-2 my-sm-0" variant="success" type="submit">Search</b-button>
         </b-nav-form>
       </b-navbar-nav>
     </b-collapse>
--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,59 +1,61 @@
 <template>
   <div>
     <div>
-  <b-jumbotron class="jumbotron">
-    <h1>Let'Go <br> to Raja Ampat</h1>
-    <p>Temukan keindahan Indonesia bersama kami</p>
-    <b-button variant="success" href="#" class="button">More Info</b-button>
-  </b-jumbotron>
-</div>
+      <b-jumbotron class="jumbotron">
+        <h1>
+          Let'Go <br />
+          to Raja Ampat
+        </h1>
+        <p>Temukan keindahan Indonesia bersama kami</p>
+        <!-- <b-button variant="success" class="btn-explore">Explore Now</b-button> -->
+        <a href="" class="btn btn-primary">Sign Up</a>
+      </b-jumbotron>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Jumbotron',
-}
+  name: "Jumbotron",
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap");
 
 .jumbotron {
   background-image: url(../assets/raja.jpg);
   background-size: cover;
   height: 860px;
+  text-align: center;
 }
 .jumbotron h1 {
-  font-family:'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   color: #fff;
   font-size: 72px;
   font-weight: 800;
-  display: flex;
-  justify-content: center;
   margin-top: 200px;
   text-shadow: 5px 7px 5px rgba(0, 0, 0, 0.45);
 }
-.jumbotron p{
+.jumbotron p {
   color: #fff;
-  font-family:'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-size: 25px;
   text-shadow: 5px 7px 5px rgba(0, 0, 0, 0.45);
-  display: flex;
-  justify-content: center;
-}
-.btn {
-  margin-left: 550px;
 }
 
 
+/* Smartphone Version */
 @media (max-width: 375px) {
-  .btn {
-  margin-left: 0;
+
+
+  .jumbotron p {
+    display: none;
+  }
 }
 
-.jumbotron p{
-  display: none;
-}
+/* Tablet Version */
+@media (max-width: 768px) {
+
 }
 </style>
